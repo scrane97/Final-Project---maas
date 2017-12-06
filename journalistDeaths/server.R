b0054db83824d51aac7fa40495c15113d35b777b
Michael Bradley <mccrazycocok@gmail.com>
2017-12-05 21:53:06 -0800

    Removed old text blocks

--- journalistDeaths/server.R
+++ journalistDeaths/server.R
@@ -37,28 +37,6 @@ shinyServer(function(input, output) {
     HTML(paste(p1, sep = '<br/>'))
   })
   
-  output$aboutProject <- renderText("For this project we looked at a dataset about journalists killed in the last 25 years.
-                          Questions that our project aims to answer include; 
-                          whether it is more common to be killed working abroad or at home, which countries are journalists most threatened by death while working, 
-                          if most journalists killed in war zones were working freelance, and is there a correlation between gender and method killed? We also aim 
-                          to show what the implications of pursuing journalism and free media are and to see if there a more strategic and common pattern of attack 
-                          against journalists. These questions and their answers are aimed to help give insight as to what level of free media comes out of these 
-                          countries and what political/social/economic implications there might be. They can also help people understand the threat against journalists 
-                          in these places. 
-                          ")
-  output$dataset <- renderText("The dataset that we will be working with is a csv. file - Journalists Killed Worldwide Since 1992 
-                  (https://www.kaggle.com/qhlearner/journalists-killed-worldwide-since-1992) from Kaggle.com. This data has been
-                  gathered from news sources from 1992 - 2016 by the Committee to Protect Journalists (CPJ). When press freedom 
-                  violations occur, CPJ has correspondents who report and take action on behalf of those targeted. They have 40 
-                  experts headquartered in NYC. CPJ reports are on violations in repressive countries, conflict zones, and 
-                  established democracies alike. A board of prominent journalists from around the world helps guide CPJ's activities.
-                  ")
-  output$analysis <- renderText("Place Analysis Here!!!!!!!")
-  output$WorldMap <- renderText("A map of deaths across the world")
-  output$Page2 <- renderText("This is page 2!!!")
-  output$genderCorrelation <- renderText("This shows the correlation of Gender and the death method")
-  
-
   # Summary and Statistics Tab
   # Creates Table and displays Table about what happened to journalists before death
     make_table <- reactive({
@@ -68,10 +46,6 @@ shinyServer(function(input, output) {
      make_table()
     })
   
-  output$deathInfo <- renderText("This table shows what the journalists experienced prior to death. As you can see, 1175 of the documented individuals have no recorded instances of torture, etc")
-
-    
-  output$AboutThisProject <- renderText("")
   output$Map <- renderLeaflet(map())
   output$pie <- renderPlotly(
     piechart(input$countries)
